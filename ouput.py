08d0e144e29d92903ed499c645846802fc8c56e1
vaibhav-033 <103326543+vaibhav-033@users.noreply.github.com>
2022-05-12 23:49:47 +0530

    Add files via upload

---
+++ ouput.py
@@ -0,0 +1,64 @@
+## Sol 1:
+
+Enter the number to be converted to binary: 56
+0b111000
+
+
+# Sol 2:
+
+Enter the expression: 2--8*6++76/4 
+69.0
+
+
+# Sol 3:
+
+Enter the number n: 3
+Enter the number r: 5
+Enter the angle a: 30
+Enter the angle b: 60
+Enter the number x1: 9
+Enter the number x2: 4
+Enter the number y1: 2 
+Enter the number y2: 7
+(3+4)(5) =  35
+n(n-1)/2 =  6.0
+4(pi)r^2 =  87.96459430051421
+sqrt(r(cosa)^2 + r(sinb)^2) =  0.7638816155960979
+y2-y1/x2-x1 =  -1.0
+
+
+# Sol 4:
+
+In the range(5)
+0
+1
+2
+3
+4
+In the range(3, 10)
+3
+4
+5
+6
+7
+8
+9
+In the range(4 ,13, 3)
+4
+7
+10
+In the range(15, 5, -2)
+15
+13
+11
+9
+7
+In the range(5, 3)
+
+
+# Sol 5:
+
+Enter the number of hydrogen atoms: 2
+Enter the number of carbon atoms: 1
+Enter the number of oxygen atoms: 3
+62.0264
